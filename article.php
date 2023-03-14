c30c4900c353c0c21c190b1faf8f3805af979675
Nicolas Terwagne <123467708+NicolasODT@users.noreply.github.com>
2023-03-14 20:17:46 +0100

    add require header php

--- article.php
+++ article.php
@@ -13,29 +13,9 @@ if (isset($_GET["slug"])) {
     $article = $query->fetch();
 
     if ($article) {
+        require_once 'header.php';
 ?>
-        <!DOCTYPE html>
-        <html lang="fr">
 
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title><?= $article["titre"] ?></title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link rel="stylesheet" href="./css/reset.css">
-            <link rel="stylesheet" href="./css/style.css">
-        </head>
-
-        <body>
-            <header>
-                <ul>
-                    <li><a href="./index.php">Accueil</a></li>
-                    <li><a href="./articles.html">Articles</a></li>
-                    <li><a href="./login.php">Connexion</a></li>
-                    <li><a href="./register.php">Inscription</a></li>
-                </ul>
-            </header>
             <main class="main-article">
     <img class="img-article" src="<?= $article["image"] ?>" alt="<?= $article["titre"] ?>">
 
