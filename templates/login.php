a09731fc359153a9384596c9d64ef4638989befe
Nicolas Terwagne <123467708+NicolasODT@users.noreply.github.com>
2023-03-18 16:31:11 +0100

    Add reCAPTCHA

--- templates/login.php
+++ templates/login.php
@@ -1,40 +1,49 @@
 <?php
 session_start();
 
+if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST['g-recaptcha-response'])) {
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
 
-if (isset($_POST["email"]) && isset($_POST["password"])) {
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
+    $recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptchaSecret = '6LeMHRElAAAAAI40BMkqAmWsQ5MaJiLizJqdp4p4';
 
+    $recaptcha = file_get_contents($recaptchaUrl . '?secret=' . $recaptchaSecret . '&response=' . $recaptchaResponse);
+    $recaptcha = json_decode($recaptcha);
 
-    require_once "../core/includes/connect.php";
+    if ($recaptcha->success) {
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
 
+        require_once "../core/includes/connect.php";
 
-    $sql = "SELECT * FROM utilisateur WHERE email LIKE :email OR pseudo LIKE :email;";
+        $sql = "SELECT * FROM utilisateur WHERE email LIKE :email OR pseudo LIKE :email;";
 
-    $query = $bdd->prepare($sql);
-    $query->bindParam(":email", $email, PDO::PARAM_STR);
-    $query->execute();
-    $results = $query->fetch();
-    if ($results) {
-        if (password_verify($password, $results['hash'])) {
-            if (!$results['actif']) {
-                header("Location: ../core/actions/deactived.php");
-                exit();
+        $query = $bdd->prepare($sql);
+        $query->bindParam(":email", $email, PDO::PARAM_STR);
+        $query->execute();
+        $results = $query->fetch();
+        if ($results) {
+            if (password_verify($password, $results['hash'])) {
+                if (!$results['actif']) {
+                    header("Location: ../core/actions/deactived.php");
+                    exit();
+                }
+                $_SESSION['email'] = $email;
+                $_SESSION['role'] = $results['role'];
+                $_SESSION['id'] = $results['id'];
+                $_SESSION['pseudo'] = $results['pseudo'];
+                header('location: ../index.php');
+            } else {
+                echo 'Mot de passe incorrect';
             }
-            $_SESSION['email'] = $email;
-            $_SESSION['role'] = $results['role'];
-            $_SESSION['id'] = $results['id'];
-            $_SESSION['pseudo'] = $results['pseudo'];
-            header('location: ../index.php');
         } else {
-            echo 'Mot de passe incorrect';
+            echo 'Email non trouvé';
         }
     } else {
-        echo 'Email non trouvé';
+        echo "Erreur reCAPTCHA";
     }
-} else {
 }
+
 require_once '../core/includes/header.php';
 ?>
 
@@ -42,11 +51,13 @@ require_once '../core/includes/header.php';
     <form action="" method="post" class="form-register">
         <input type="text" name="email" placeholder="Email ou Pseudo*" require>
         <input type="password" name="password" placeholder="Mot de passe *" require>
+        <div class="g-recaptcha" data-sitekey="6LeMHRElAAAAACgEgUGBNqALUexnqykdbjJq7z-O"></div>
         <button>Connexion</button>
     </form>
 </main>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 <?php
-
 require_once '../core/includes/footer.php';
-
 ?>
