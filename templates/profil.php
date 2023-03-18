0093158547c51a5c18423572335c4aa838913ee3
Nicolas Terwagne <123467708+NicolasODT@users.noreply.github.com>
2023-03-18 21:40:00 +0100

    add pagination commentaire-profil

--- templates/profil.php
+++ templates/profil.php
@@ -14,9 +14,15 @@ $stmt->execute([$_SESSION['id']]);
 $utilisateur = $stmt->fetch();
 
 // Récupère les commentaires de l'utilisateur
-$sql = "SELECT c.*, a.titre AS article_titre, a.image AS article_image FROM Commentaire c JOIN Article a ON c.id_article = a.id WHERE c.id_utilisateur = :id_utilisateur ORDER BY c.date_creation DESC";
+$limit = 4;
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
+$offset = ($page - 1) * $limit;
+
+$sql = "SELECT c.*, a.titre AS article_titre, a.image AS article_image FROM Commentaire c JOIN Article a ON c.id_article = a.id WHERE c.id_utilisateur = :id_utilisateur ORDER BY c.date_creation DESC LIMIT :limit OFFSET :offset";
 $stmt = $bdd->prepare($sql);
 $stmt->bindParam(":id_utilisateur", $_SESSION['id']);
+$stmt->bindParam(":limit", $limit, PDO::PARAM_INT);
+$stmt->bindParam(":offset", $offset, PDO::PARAM_INT);
 $stmt->execute();
 $commentaires = $stmt->fetchAll();
 
@@ -38,21 +44,21 @@ require_once '../core/includes/header.php';
 
 ?>
 
-    <h1>Profil de <?= $utilisateur['pseudo'] ?></h1>
-    <form action="" method="post" class="form-profil">
-        <label for="E-mail">E-mail</label>
-        <input type="email" name="email" id="email" value="<?=$utilisateur['email']?>" disabled>
-        <label for="password">Nouveau mot de passe:</label>
-        <input type="password" name="password" id="password" placeholder="*********" required>
-        <label for="pseudo">Pseudo</label>
-        <input type="pseudo" name="pseudo" id="pseudo" value="<?=$utilisateur['pseudo']?>" disabled>
-        <label for="ville">Ville:</label>
-        <input type="text" name="ville" id="ville" value="<?= $utilisateur['ville'] ?>">
-        <label for="pays">Pays:</label>
-        <input type="text" name="pays" id="pays" value="<?= $utilisateur['pays'] ?>">
-        <button name="submit">Enregistrer</button>
-    </form>
-    <section class="commentaires">
+<h1>Profil de <?= $utilisateur['pseudo'] ?></h1>
+<form action="" method="post" class="form-profil">
+    <label for="E-mail">E-mail</label>
+    <input type="email" name="email" id="email" value="<?=$utilisateur['email']?>" disabled>
+    <label for="password">Nouveau mot de passe:</label>
+    <input type="password" name="password" id="password" placeholder="*********" required>
+    <label for="pseudo">Pseudo</label>
+    <input type="pseudo" name="pseudo" id="pseudo" value="<?=$utilisateur['pseudo']?>" disabled>
+    <label for="ville">Ville:</label>
+    <input type="text" name="ville" id="ville" value="<?= $utilisateur['ville'] ?>">
+    <label for="pays">Pays:</label>
+    <input type="text" name="pays" id="pays" value="<?= $utilisateur['pays'] ?>">
+    <button name="submit">Enregistrer</button>
+</form>
+<section class="commentaires">
     <h2>Vos commentaires</h2>
     <?php
     if (count($commentaires) > 0) {
@@ -71,7 +77,16 @@ require_once '../core/includes/header.php';
     }
     ?>
 </section>
-    <?php
-    require_once '../core/includes/footer.php';
-    ?>
 
+<div class="pagination">
+    <?php if ($page > 1): ?>
+        <a href="?page=<?= $page - 1 ?>" class="pagination-prev"> précédents</a>
+    <?php endif; ?>
+    <?php if (count($commentaires) == $limit): ?>
+        <a href="?page=<?= $page + 1 ?>" class="pagination-next"> suivants</a>
+    <?php endif; ?>
+</div>
+
+<?php
+require_once '../core/includes/footer.php';
+?>
